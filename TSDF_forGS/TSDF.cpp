d23b9b05b264d1a997777a02a5dc425dc04974dd
DonaldTrump-coder <3279179450@qq.com>
2025-09-04 12:20:32 +0800

    change params

--- TSDF_forGS/TSDF.cpp
+++ TSDF_forGS/TSDF.cpp
@@ -125,17 +125,17 @@ void Grids::Gaussian_Integration(Gaussian& gs)
                     continue;
                 if(k<0||k>z_length)
                     continue;
-                if(index_scale>10)
+                if(index_scale>8)
                     continue;
                 Vertex* vert=get_vertex(i,j,k);// The vertex to be projected
                 glm::vec3 vect(vert->x-gs.means.x , vert->y-gs.means.y , vert->z-gs.means.z); //The vector from Gaussian center to Vertex
                 float u_scale = abs(dot(gs.u , vect));
                 float v_scale = abs(dot(gs.v , vect));
                 float normalized_dist = (u_scale/gs.scale.x)*(u_scale/gs.scale.x)+(v_scale/gs.scale.y)*(v_scale/gs.scale.y);
-                if(normalized_dist>4)//temporary parameters
+                if(normalized_dist>3)//temporary parameters
                     continue; //the Vertex is out of bound of the Gaussian
                 float weight = abs(gs.opacity * exp(-0.5*normalized_dist));
-                if(weight<0.02)
+                if(weight<0.05)
                     continue;
 
                 float GaussianDF = dot(vect , gs.normal);
