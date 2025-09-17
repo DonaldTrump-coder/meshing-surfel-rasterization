744791e259a58b4e8f6ccc272493975c325aac66
DonaldTrump-coder <3279179450@qq.com>
2025-09-17 14:00:21 +0800

    fo

--- TSDF_forGS/TSDF.cpp
+++ TSDF_forGS/TSDF.cpp
@@ -132,7 +132,7 @@ void Grids::Gaussian_Integration(Gaussian& gs)
                 float u_scale = abs(dot(gs.u , vect));
                 float v_scale = abs(dot(gs.v , vect));
                 float normalized_dist = (u_scale/gs.scale.x)*(u_scale/gs.scale.x)+(v_scale/gs.scale.y)*(v_scale/gs.scale.y);
-                if(normalized_dist>2)//temporary parameters
+                if(normalized_dist>4)//temporary parameters
                     continue; //the Vertex is out of bound of the Gaussian
                 float weight = abs(1.6*gs.opacity * exp(-0.5*normalized_dist));
                 if(weight<0.001)
